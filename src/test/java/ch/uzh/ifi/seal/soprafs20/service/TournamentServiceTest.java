f29c88d6fdda8981a2d98c693660fb7cbc48296d
maurohirt <45426145+maurohirt@users.noreply.github.com>
2020-05-22 14:50:20 +0200

    FS20G01-137 more tournanament service tests

--- src/test/java/ch/uzh/ifi/seal/soprafs20/service/TournamentServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/service/TournamentServiceTest.java
@@ -33,43 +33,43 @@ class TournamentServiceTest {
 
 	@Mock
 	private TournamentRepository tournamentRepository;
-	
+
 	@Mock
     private GameRepository gameRepository;
-    
+
 	@Mock
 	private BracketRepository bracketRepository;
-    
+
 	@Mock
 	private LeaderboardRepository leaderboardRepository;
 
     @InjectMocks
     private TournamentService tournamentService;
-    
+
     private Participant testParticipant1;
     private Participant testParticipant2;
     private Participant testParticipant3;
-    
+
     private Tournament testTournament1;
     private Tournament testTournament2;
-       
+
     private Bracket testBracket1;
     private Bracket testBracket2;
-    
+
     private Game testGame1;
     private Game testGame2;
-    
+
     private Leaderboard testLeaderboard1;
-    
+
     @BeforeEach
-    public void setup() {
+     void setup() {
     	 MockitoAnnotations.initMocks(this);
 
          // given
          testParticipant1 = new Participant();
          testParticipant2 = new Participant();
          testParticipant3 = new Participant();
-         
+
          List<Participant> dummyList1 = new ArrayList<>();
 
          testParticipant1.setVorname("Fabio");
@@ -87,17 +87,17 @@ class TournamentServiceTest {
          testParticipant3.setNachname("Ly");
          testParticipant3.setPassword("apple");
          testParticipant3.setLicenseNumber("654321");
-         
+
          testTournament1 = new Tournament();
          testTournament2 = new Tournament();
-         
+
          testBracket1 = new Bracket();
          testBracket2 = new Bracket();
-         
+
          testLeaderboard1 = new Leaderboard();
-         
+
          List<Tournament> dummyList2 = new ArrayList<>();
-         
+
          testTournament1.setAmountOfPlayers(4);
          testTournament1.setBracket(testBracket1);
          testTournament1.setBreakDuration(10);
@@ -110,7 +110,7 @@ class TournamentServiceTest {
          testTournament1.setStartTime("12:00");
          testTournament1.setTournamentName("NAME1");
          testTournament1.setWinner(testParticipant1);
-         
+
          testTournament2.setAmountOfPlayers(8);
          testTournament2.setBracket(testBracket2);
          testTournament2.setBreakDuration(5);
@@ -123,12 +123,12 @@ class TournamentServiceTest {
          testTournament2.setStartTime("22:00");
          testTournament2.setTournamentName("NAME2");
          testTournament2.setWinner(testParticipant2);
-         
+
          List<Game> dummyList3 = new ArrayList<>();
-         
+
          testGame1 = new Game();
          testGame2 = new Game();
-         
+
          testGame1.setStartTime("10:00");
          testGame1.setGameState(GameState.FINISHED);
          testGame1.setScore1(3);
@@ -136,7 +136,7 @@ class TournamentServiceTest {
          testGame1.setParticipant1(testParticipant1);
          testGame1.setParticipant2(testParticipant2);
          testGame1.setTournamentCode("TEST1");
-         
+
          testGame2.setStartTime("14:00");
          testGame2.setGameState(GameState.READY);
          testGame2.setScore1(2);
@@ -144,19 +144,19 @@ class TournamentServiceTest {
          testGame2.setParticipant1(testParticipant3);
          testGame2.setParticipant2(testParticipant2);
          testGame2.setTournamentCode("TEST1");
-         
+
          dummyList3.add(testGame1);
          dummyList3.add(testGame2);
-         
+
          dummyList2.add(testTournament1);
          dummyList2.add(testTournament2);
-         
+
          dummyList1.add(testParticipant1);
          dummyList1.add(testParticipant2);
          dummyList1.add(testParticipant3);
     }
     @Test
-    public void createTournamentSuccessShouldHaveTournamentCode(){
+     void createTournamentSuccessShouldHaveTournamentCode(){
         Tournament createdTournament = tournamentService.createTournament(testTournament1);
 
         assertNotNull(createdTournament.getTournamentCode()); // random generated tournament code
@@ -173,7 +173,7 @@ class TournamentServiceTest {
     }
 
     @Test
-    public void createBracketSuccessShouldReturnBracket() {
+     void createBracketSuccessShouldReturnBracket() {
         int numberOfPlayers = 8;
         String tournamentCode = "TEST2";
         int numberOfTables = 4;
@@ -190,7 +190,7 @@ class TournamentServiceTest {
 
 
     @Test
-    public void updateGameWithScoreNoSuccessWhenGameLockedRaiseException() throws Exception{
+     void updateGameWithScoreNoSuccessWhenGameLockedRaiseException() throws Exception{
 
         Mockito.when(gameRepository.findByGameId(Mockito.anyLong())).thenReturn(testGame1); // testGame1 is finished
 
@@ -199,7 +199,35 @@ class TournamentServiceTest {
     }
 
     @Test
-    public void createLeaderboardEntryNoSuccessWhenTournamentFullShouldRaiseException() throws Exception{
+     void createLeaderboardEntryNoSuccessWhenParticipantlreadyInLeaderboardException() throws Exception{
+        List<Leaderboard> list = new ArrayList<>();
+
+            Leaderboard leaderboardTestParticipant1 = new Leaderboard();
+            leaderboardTestParticipant1.setParticipant(testParticipant1);
+            list.add(leaderboardTestParticipant1);
+
+
+        Mockito.when(leaderboardRepository.findAllByTournamentCode(Mockito.any())).thenReturn(list);
+
+        assertThrows(ResponseStatusException.class, () -> { tournamentService.createLeaderboardEntry(testParticipant1, testTournament2); });
+    }
+    /**
+     *check if no exception is thrown because then the method  leaderboardRepository.save(leaderboard) is called and the new participant is saved to the repository
+     *
+     */
+
+    @Test
+     void createLeaderboardEntrySuccess() {
+        List<Leaderboard> list = new ArrayList<>();
+        Leaderboard leaderboardTestParticipant1 = new Leaderboard();        leaderboardTestParticipant1.setParticipant(testParticipant1);
+        list.add(leaderboardTestParticipant1);
+
+
+        Mockito.when(leaderboardRepository.findAllByTournamentCode(Mockito.any())).thenReturn(list);
+        tournamentService.createLeaderboardEntry(testParticipant2, testTournament2);
+    }
+    @Test
+     void createLeaderboardEntryNoSuccessWhenTournamentFullShouldRaiseException() throws Exception{
 
         List<Leaderboard> list = new ArrayList<>();
 
@@ -212,9 +240,63 @@ class TournamentServiceTest {
         assertThrows(ResponseStatusException.class, () -> { tournamentService.createLeaderboardEntry(testParticipant1, testTournament2); });
 
     }
+    //these  get methods will not be tested
+    /*
+    public Tournament getTournamentByTournamentCode(String tournamentCode) {
+    }
+
+    public List<Game> getBracketByTournamentCode(String tournamentCode) {
+    }
+
+    public List<Tournament> getAllTournaments() {
+
+    }
+
+    public List<Leaderboard> getLeaderboardFromTournament(String tournamentCode) {
+    }
+    */
+    @Test
+     void checkIfTournamentCodeExistsPositiveTest() {
+
+        Mockito.when(tournamentRepository.findByTournamentCode(Mockito.any())).thenReturn(testTournament1);
+        assertTrue(tournamentService.checkIfTournamentCodeExists(Mockito.any()));
+
+
+    }
+    @Test
+     void checkIfTournamentCodeExistsNegativeTest() {
+
+        Mockito.when(tournamentRepository.findByTournamentCode(Mockito.any())).thenReturn(null);
+        assertFalse(tournamentService.checkIfTournamentCodeExists(Mockito.any()));
+
+
+    }
 
     @Test
-    public void updateBracketAndLeaderboardAfterUserLeftSuccessPlayerStateLEFT() {
+     void checkIfParticipantIsInLeaderboardNegativeTest() {
+        List<Leaderboard> list = new ArrayList<>();
+        String testLicenceNumber = "1234";
+
+        Mockito.when(leaderboardRepository.findAllByTournamentCode(Mockito.any())).thenReturn(list);
+        assertFalse(tournamentService.checkIfParticipantIsInLeaderboard(testLicenceNumber,Mockito.any()));
+
+
+    }
+    @Test
+    void checkIfParticipantIsInLeaderboardositiveTest() {
+        List<Leaderboard> list = new ArrayList<>();
+        Leaderboard testleaderBoard = new Leaderboard();
+        testleaderBoard.setParticipant(testParticipant1);
+        list.add(testleaderBoard);
+        list.add(testLeaderboard1);
+
+        Mockito.when(leaderboardRepository.findAllByTournamentCode(Mockito.any())).thenReturn(list);
+        assertTrue(tournamentService.checkIfParticipantIsInLeaderboard("112233",Mockito.any()));
+
+
+    }
+    @Test
+    void updateBracketAndLeaderboardAfterUserLeftSuccessPlayerStateLEFT() {
         Leaderboard leaderboardOfTestParticipant1 = new Leaderboard();
         leaderboardOfTestParticipant1.setLosses(4);
         leaderboardOfTestParticipant1.setParticipant(testParticipant1);
